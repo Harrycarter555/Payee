70cb14e44dca0606557255dfe0d72a9185f1ff44
Harrycarter555 <top9pdf@gmail.com>
2024-07-31 21:25:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
-TELEGRAM_TOKEN = '7370642028:AAFiTg3L4T7sLEZeRuaodmlhiwVsXZ5LeKE'
+TELEGRAM_TOKEN = '7122789486:AAGk_EDRtsR3kbY7WTEE13HRbNRWZ2Refh4'
 WEBHOOK_URL = 'https://storagehc.vercel.app/webhook'
